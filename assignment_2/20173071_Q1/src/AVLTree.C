640c4042376ac109b334f68ba7b3ecf2ff7f4173
Aditya Saripalli <aditya.saripalli@students.iiit.ac.in>
2020-10-05 02:56:40 +0530

    A-2:Added complete logic for Insertion/Deletion in AVL trees.

--- assignment_2/20173071_Q1/src/AVLTree.C
+++ assignment_2/20173071_Q1/src/AVLTree.C
@@ -5,25 +5,6 @@
 #include <iostream>
 using namespace std;
 
-void printTree(const std::string& prefix, const Node<int>* node, bool isLeft)
-{
-    if( node != nullptr )
-    {
-        std::cout << prefix;
-        std::cout << (isLeft ? "├──" : "└──" );
-        // print the value of the node
-        std::cout << node->key << "[" << node->balance << "]" << std::endl;
-        // enter the next tree level - left and right branch
-        printTree( prefix + (isLeft ? "│   " : "    "), node->left, true);
-        printTree( prefix + (isLeft ? "│   " : "    "), node->right, false);
-    }
-}
-
-void printTree(const Node<int>* node)
-{
-    printTree("", node, false);
-}
-
 
 int main (int argc, char* argv[])
 {
@@ -36,19 +17,24 @@ int main (int argc, char* argv[])
     sAVLTree.insertNode(5);
     sAVLTree.insertNode(15);
     sAVLTree.insertNode(55);
-    sAVLTree.insertNode(5);
     sAVLTree.insertNode(65);
-    sAVLTree.insertNode(60);
+    sAVLTree.insertNode(65);
     sAVLTree.insertNode(75);
     sAVLTree.insertNode(32);
-    sAVLTree.insertNode(32);
+    sAVLTree.insertNode(20);
     sAVLTree.insertNode(12);
-    sAVLTree.insertNode(55);
     sAVLTree.insertNode(60);
-    printTree(sAVLTree.getRootNode());
+    sAVLTree.insertNode(60);
+    sAVLTree.insertNode(32);
+    sAVLTree.insertNode(15);
+    sAVLTree.display();
+    sAVLTree.deleteNode(55);
+    sAVLTree.display();
     sAVLTree.deleteNode(60);
-    sAVLTree.deleteNode(32);
-    printTree(sAVLTree.getRootNode());
+    sAVLTree.display();
+    sAVLTree.deleteNode(20);
+    sAVLTree.display();
+    sAVLTree.print();
 
     return 0;
 }
